287eab32e903f230c469e2fb1c457612e41cb557
nimisane <nimishsane9@gmail.com>
2021-11-07 13:59:29 +0530

    video player added

---
+++ lib/video_player.dart
@@ -0,0 +1,58 @@
+import 'package:chewie/chewie.dart';
+import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
+
+class VideoPlayer extends StatefulWidget {
+  const VideoPlayer({Key? key}) : super(key: key);
+
+  @override
+  _VideoPlayerState createState() => _VideoPlayerState();
+}
+
+class _VideoPlayerState extends State<VideoPlayer> {
+  late ChewieController chewieController;
+  final videoPlayerController = VideoPlayerController.network(
+      'https://flutter.github.io/assets-for-api-docs/assets/videos/butterfly.mp4');
+
+  @override
+  initState() {
+    videoPlayerController.initialize();
+    chewieController = ChewieController(
+      videoPlayerController: videoPlayerController,
+      aspectRatio: 16 / 9,
+      autoPlay: true,
+      looping: true,
+    );
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var deviceHeight = MediaQuery.of(context).size.height;
+    var deviceWidth = MediaQuery.of(context).size.width;
+    return Scaffold(
+      body: Container(
+        width: deviceWidth,
+        child: Column(
+          children: [
+            SizedBox(
+              height: 50,
+            ),
+            Container(
+              width: deviceWidth,
+              height: 250,
+              child: Chewie(
+                controller: chewieController,
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text('Play the video'),
+          ],
+        ),
+      ),
+    );
+  }
+}
